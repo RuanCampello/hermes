a7d3ee45b199609110745b8beeb13459317f284a
RuanCampello <ruancampello999@gmail.com>
2025-07-26 20:59:46 -0300

    feat: more complete device interface

--- src/physical/mod.rs
+++ src/physical/mod.rs
@@ -1,11 +1,46 @@
 //! Physical layer (or PHY), the first part of the networking stack.
 //! This the most low-level and responsible of direct interaction with hardware.
 
+use crate::time::Instant;
+
 /// Metadata of a packet.
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct PacketMetadata {
     pub id: u32,
+}
+
+/// Describe a given device's capabilities.
+pub struct DeviceCapabilities {
+    /// The device's network medium type.
+    ///
+    /// Determines packet format and interface behavior. For example, ARP/NDISC is only
+    /// performed for [`Medium::Ethernet`].
+    pub medium: Medium,
+    /// Maximum transmission unit (MTU) in octets.  
+    ///  
+    /// The device cannot send/receive frames larger than this.  
+    ///  
+    /// For Ethernet, this is the max frame size *including* the 14-octet header  
+    /// but *excluding* the 4-octet FCS. Thus, Ethernet MTU = IP MTU + 14.  
+    ///  
+    /// Note: Linux/other OSes define "MTU" as the IP MTU, even for Ethernet.  
+    pub max_transmission_unit: usize,
+    /// Maximum burst size (in MTU units).  
+    ///  
+    /// The device cannot send/receive bursts larger than this.  
+    pub max_burst_size: Option<usize>,
+    /// Checksum behaviour.
+    pub checksum: ChecksumCapabilities,
+}
+
+/// Describe the checksum behaviour for each protocol.
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub struct ChecksumCapabilities {
+    pub ipv4: Checksum,
+    pub udp: Checksum,
+    pub tcp: Checksum,
 }
 
 /// Checksum behaviour of a given protocol.
@@ -23,6 +58,14 @@ pub enum Checksum {
     Tx,
 }
 
+/// Type of a medium device.
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub enum Medium {
+    Ethernet,
+    Ip,
+}
+
 /// Interface for sending and receiving raw frames.
 pub trait Device {
     type RxToken<'t>: RxToken
@@ -32,8 +75,14 @@ pub trait Device {
     where
         Self: 't;
 
-    fn receive(&mut self) -> Option<(Self::RxToken<'_>, Self::TxToken<'_>)>;
-    fn transmit(&mut self) -> Option<Self::TxToken<'_>>;
+    /// Creates a receiving token and a transmit token.
+    fn receive(&mut self, timestamp: Instant) -> Option<(Self::RxToken<'_>, Self::TxToken<'_>)>;
+
+    /// Creates a transmit token.
+    fn transmit(&mut self, timestamp: Instant) -> Option<Self::TxToken<'_>>;
+
+    /// Describe the device capabilities.
+    fn capabilities(&self) -> Option<()>;
 }
 
 pub trait RxToken {}
